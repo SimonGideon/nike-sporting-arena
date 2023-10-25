a1776952cb470d9e9692ec1e125a334a68eea10a
SimonGideon <simongideon918@gmail.com>
2023-10-25 11:30:20 +0300

    Initial commit

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,13 +21,38 @@ import watch from './icons/Stop-watch.svg';
 import twitter from './icons/Twitter.svg';
 import vans from './icons/Vans.svg';
 import vector from './icons/Vector.svg';
-// 
 
-// 
+// products
+import desk from './products/desk/Desk.svg';
+import product_1 from './products/desk/Product1.svg';
+import product_2 from './products/desk/Product2.svg';
+import product_3 from  './products/desk/Product2.svg';
+import product_4 from './products/desk/Product4.svg';
+import product_5 from './products/desk/Product5.svg';
+import product_6 from './products/desk/Product6.svg';
+
+// hero
+import hero_1 from './products/hero/hero-1.png';
+import hero_2 from './products/hero/hero-2.png';
+import hero_3 from './products/hero/hero-3.svg';
+import hero_4 from './products/hero/hero-4.svg';
+
+// others
 
 export {
   logo,
+  product_1,
+  product_2,
+  product_3,
+  product_4,
+  product_5,
+  product_6,
+  hero_1,
+  hero_2,
+  hero_3,
+  hero_4,
   vector,
+  desk,
   vans,
   loader,
   nike_icon,
