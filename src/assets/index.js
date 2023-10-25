1b05ecf300c8a5080f2f6cdc04f42ec9fe231e53
SimonGideon <simongideon918@gmail.com>
2023-10-25 11:20:18 +0300

    load all import icons

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,51 @@
 import loader from './loader.svg';
-import logo from './logo.svg';
+// icons
+import logo from './Logo.svg';
+import nike_icon from './icons/_nike_icon.png';
+import account from './icons/Account.svg';
+import arrow from './icons/Arrow.svg';
+import cube from './icons/Cube.svg';
+import instagram from './icons/instagram.svg';
+import adidas from './icons/Adidas.svg';
+import nike from './icons/Nike.svg';
+import converse from './icons/Converse.svg';
+import facebook from './icons/Facebook.svg';
+import nb from './icons/Nb.svg';
+import jordan from './icons/Jordan.svg';
+import reebok from './icons/Reebok.svg';
+import search from './icons/Search.svg';
+import shield from './icons/Shield.svg';
+import shiping_track from './icons/Shiping_track.svg';
+import shopping from './icons/Shopping-bag.svg';
+import watch from './icons/Stop-watch.svg';
+import twitter from './icons/Twitter.svg';
+import vans from './icons/Vans.svg';
+import vector from './icons/Vector.svg';
+// 
+
+// 
 
 export {
   logo,
+  vector,
+  vans,
   loader,
+  nike_icon,
+  arrow,
+  account,
+  twitter,
+  nb,
+  shopping,
+  search,
+  jordan,
+  reebok,
+  cube,
+  watch,
+  shield,
+  shiping_track,
+  instagram,
+  adidas,
+  nike,
+  converse,
+  facebook,
 };
