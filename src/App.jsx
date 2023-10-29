a0875f64367a4713a3381ef611f9356512228ff2
SimonGideon <simongideon918@gmail.com>
2023-10-29 16:10:38 +0300

    Add Hamburger button

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,18 @@ import {  Home, Navbar } from './components';
 const App = () => {
 
   return (
-    <div className="relative flex">
-      <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
-        <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
-          <div className="flex-1 h-fit pb-40">
+    <div className="flex bg-black">
+      <div className="">
+        <div className="">
+        <div className="">
+            <Navbar />
+          </div>
+          <div className="">
             <Routes>
               <Route path="/" element={<Home />} />
             </Routes>
           </div>
-          <div className=" relative top-0 w-fit ">
-            <Navbar />
-          </div>
+          
         </div>
       </div>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,22 @@
-
-import React from 'react';
-import { links } from '../assets/constants';
-const Navbar = () => { 
-  return(
-  <div>
-    {
-    links.map((link, i) => (
-      <li key={i}>
-        <a href={link.to}>{link.name}</a>
-        </li>
-    ))
-    }
-  </div>
-)};
+import React from "react";
+import { useState } from "react";
+import { links } from "../assets/constants";
+import { Turn as Hamburger } from 'hamburger-react'
+import "./styles.css";
+const Navbar = () => {
+  const [isOpen, setOpen] = useState(false)
+  return (
+    <div className="flex bg-red-400 text-white w-screen ">
+      <Hamburger toggled={isOpen} toggle={setOpen} />
+      <div className="flex justify-between">
+        {links.map((link, i) => (
+          <li key={i} className="list-none ">
+            <a href={link.to}>{link.name}</a>
+          </li>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
